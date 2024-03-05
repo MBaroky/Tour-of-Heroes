1f4abb030820f3c566c9c301b1849970d22c57b6
Mohamed Gamal El Baroky <m.baroky@gmail.com>
2024-03-05 23:52:59 +0200

    hero component, interface, uppercasepipe and 2-way binding

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+  hero: Hero = {'id':0, name:'Windstorm'};
+}
