9104f0a5a4df7e3b62e105a45f23c4304ad84db3
Mohamed Gamal El Baroky <m.baroky@gmail.com>
2024-03-16 23:06:09 +0200

    add service for heroes adata, next: observables

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {HEROES} from '../mock-heroes';
+import { HeroService } from '../hero.service';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 @Component({
   selector: 'app-heroes',
@@ -12,9 +12,16 @@ import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+  getHeroes():void{
+    this.heroes = this.heroService.getHeroes();
+  }
+  ngOnInit():void{
+    this.getHeroes()
+  }
+  constructor(private heroService:HeroService){}
 }
